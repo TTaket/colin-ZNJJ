4d37d98ae95545d8d8ceac185fea34dad3f0fb2d
LJY <107829564+TTaket@users.noreply.github.com>
2024-11-08 20:43:37 +0800

    feat: 丰富客户端服务端 添加了多客户端连接与相互通讯

--- client/client.cpp
+++ client/client.cpp
@@ -6,32 +6,45 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <pthread.h>
 
+void* readMessages(void* arg) {
+    SOCKNODE* sock = (SOCKNODE*)arg;
+    char buf[100] = {};
+    while (true) {
+        memset(buf, '\0', sizeof buf);
+        int len = recvMsg(sock, buf, sizeof(buf));
+        if (len == 0) {
+            printf("server close\n");
+            break;
+        }
+        printf("recv from server: %s\n", buf);
+    }
+    return nullptr;
+}
 
 int main(){
-    SOCKNODE* sock = createSocket("zhu",strlen("zhu"));
+    SOCKNODE* sock = createSocket("zhu", strlen("zhu"));
     ConnSocket(sock, SERVER_IP, SERVER_PORT);
     FullSocketInfo(sock);
     ERROR_CHECK_AND_CLOSE;
 
-    // 收发数据
+    pthread_t readerThread;
+    pthread_create(&readerThread, nullptr, readMessages, sock);
+
     char buf[100] = {};
-    setBlock(sock);
-    while(1){
-        printf("-----------------\n");
+    
+    while (true) {
         memset(buf, '\0', sizeof buf);
-        fgets(buf, sizeof(buf), stdin);
+        //缺少输入检查
+        scanf("%99[^\n]", buf);
+        getchar();
         sendMsg(sock, buf, strlen(buf));
-        int len = recvMsg(sock, buf , strlen(buf));
-        if(len == 0){
-            printf("server close\n");
-            break;
-        }
-        printf("recv from server :%s" , buf);
     }
 
+    pthread_join(readerThread, nullptr);
 
-    // 关闭
+    // Close the socket
     closeSocket(sock);
     return 0;
 }
