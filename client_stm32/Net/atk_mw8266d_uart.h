f6e3fc42673fce47ae235efbdd028ec18e67953e
LJY <107829564+TTaket@users.noreply.github.com>
2024-11-20 13:55:04 +0800

    docs: update

--- client_stm32/Net/atk_mw8266d_uart.h
+++ client_stm32/Net/atk_mw8266d_uart.h
@@ -1,45 +1,46 @@
 /**
  ****************************************************************************************************
  * @file        atk_mw8266d_uart.h
- * @author      ÕıµãÔ­×ÓÍÅ¶Ó(ALIENTEK)
+ * @author      ï¿½ï¿½ï¿½ï¿½Ô­ï¿½ï¿½ï¿½Å¶ï¿½(ALIENTEK)
  * @version     V1.0
  * @date        2022-06-21
- * @brief       ATK-MW8266DÄ£¿éUART½Ó¿ÚÇı¶¯´úÂë
- * @license     Copyright (c) 2020-2032, ¹ãÖİÊĞĞÇÒíµç×Ó¿Æ¼¼ÓĞÏŞ¹«Ë¾
+ * @brief       ATK-MW8266DÄ£ï¿½ï¿½UARTï¿½Ó¿ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+ * @license     Copyright (c) 2020-2032, ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ó¿Æ¼ï¿½ï¿½ï¿½ï¿½Ş¹ï¿½Ë¾
  ****************************************************************************************************
  * @attention
  *
- * ÊµÑéÆ½Ì¨:ÕıµãÔ­×Ó MiniSTM32 V4¿ª·¢°å
- * ÔÚÏßÊÓÆµ:www.yuanzige.com
- * ¼¼ÊõÂÛÌ³:www.openedv.com
- * ¹«Ë¾ÍøÖ·:www.alientek.com
- * ¹ºÂòµØÖ·:openedv.taobao.com
+ * Êµï¿½ï¿½Æ½Ì¨:ï¿½ï¿½ï¿½ï¿½Ô­ï¿½ï¿½ MiniSTM32 V4ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+ * ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æµ:www.yuanzige.com
+ * ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ì³:www.openedv.com
+ * ï¿½ï¿½Ë¾ï¿½ï¿½Ö·:www.alientek.com
+ * ï¿½ï¿½ï¿½ï¿½ï¿½Ö·:openedv.taobao.com
  *
  ****************************************************************************************************
  */
 
 #include "stdint.h"
-/* Òı½Å¶¨Òå */
+/* ï¿½ï¿½ï¿½Å¶ï¿½ï¿½ï¿½ */
+//åœ¨è¿™é‡Œæ”¹å˜ä¸²å£
 #define ATK_MW8266D_UART_TX_GPIO_PORT           GPIOA
 #define ATK_MW8266D_UART_TX_GPIO_PIN            GPIO_PIN_3
-#define ATK_MW8266D_UART_TX_GPIO_CLK_ENABLE()   do{ __HAL_RCC_GPIOA_CLK_ENABLE(); }while(0) /* PC¿ÚÊ±ÖÓÊ¹ÄÜ */
+#define ATK_MW8266D_UART_TX_GPIO_CLK_ENABLE()   do{ __HAL_RCC_GPIOA_CLK_ENABLE(); }while(0) /* PCï¿½ï¿½Ê±ï¿½ï¿½Ê¹ï¿½ï¿½ */
 
 #define ATK_MW8266D_UART_RX_GPIO_PORT           GPIOA
 #define ATK_MW8266D_UART_RX_GPIO_PIN            GPIO_PIN_2
-#define ATK_MW8266D_UART_RX_GPIO_CLK_ENABLE()   do{ __HAL_RCC_GPIOA_CLK_ENABLE(); }while(0) /* PD¿ÚÊ±ÖÓÊ¹ÄÜ */
+#define ATK_MW8266D_UART_RX_GPIO_CLK_ENABLE()   do{ __HAL_RCC_GPIOA_CLK_ENABLE(); }while(0) /* PDï¿½ï¿½Ê±ï¿½ï¿½Ê¹ï¿½ï¿½ */
 
-/* UARTÊÕ·¢»º³å´óĞ¡ */
+/* UARTï¿½Õ·ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ğ¡ */
 #define ATK_MW8266D_UART_RX_BUF_SIZE            128
 #define ATK_MW8266D_UART_TX_BUF_SIZE            64
 
 
-extern uint8_t g_uart_tx_buf[ATK_MW8266D_UART_TX_BUF_SIZE]; /* ATK-MW8266D UART·¢ËÍ»º³å */
+extern uint8_t g_uart_tx_buf[ATK_MW8266D_UART_TX_BUF_SIZE]; /* ATK-MW8266D UARTï¿½ï¿½ï¿½Í»ï¿½ï¿½ï¿½ */
 
-/* ²Ù×÷º¯Êı */
+/* ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ */
 void atk_mw8266d_uart_printf(char *fmt, ...);       /* ATK-MW8266D UART printf */
-void atk_mw8266d_uart_rx_restart(void);             /* ATK-MW8266D UARTÖØĞÂ¿ªÊ¼½ÓÊÕÊı¾İ */
-uint8_t *atk_mw8266d_uart_rx_get_frame(void);       /* »ñÈ¡ATK-MW8266D UART½ÓÊÕµ½µÄÒ»Ö¡Êı¾İ */
-uint16_t atk_mw8266d_uart_rx_get_frame_len(void);   /* »ñÈ¡ATK-MW8266D UART½ÓÊÕµ½µÄÒ»Ö¡Êı¾İµÄ³¤¶È */
+void atk_mw8266d_uart_rx_restart(void);             /* ATK-MW8266D UARTï¿½ï¿½ï¿½Â¿ï¿½Ê¼ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ */
+uint8_t *atk_mw8266d_uart_rx_get_frame(void);       /* ï¿½ï¿½È¡ATK-MW8266D UARTï¿½ï¿½ï¿½Õµï¿½ï¿½ï¿½Ò»Ö¡ï¿½ï¿½ï¿½ï¿½ */
+uint16_t atk_mw8266d_uart_rx_get_frame_len(void);   /* ï¿½ï¿½È¡ATK-MW8266D UARTï¿½ï¿½ï¿½Õµï¿½ï¿½ï¿½Ò»Ö¡ï¿½ï¿½ï¿½İµÄ³ï¿½ï¿½ï¿½ */
 
 /*user*/
 int atk_mw8266d_uart_rx_get_frame_Isableread();
